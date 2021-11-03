bb5746897e942cd7e354f48eb1b78d41a197f8b1
Sean Scally <scally@users.noreply.github.com>
2021-11-03 16:52:13 -0700

    Fix warnings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import '../styles/globals.css'
 import '../styles/Tichu.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Head>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>)
+
 }
 
 export default MyApp
