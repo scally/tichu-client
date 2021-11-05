190cc4e89ab1b9e27ca50ec294db3320e0b7b612
Sean Scally <scally@users.noreply.github.com>
2021-11-04 19:14:21 -0700

    React devtools in dev mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+export async function getStaticProps() {
+  return { 
+    isDevelopment: process.env.NODE_ENV === 'development'
+  }
+}
+
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          {
+            this.props.isDevelopment && <script src="http://localhost:8097"></script>
+          }          
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         </Head>
